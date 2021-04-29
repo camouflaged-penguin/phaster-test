963f19deadaaf6571c99b4f1216f000c2f5a0556
camouflaged-penguin <82024665+camouflaged-penguin@users.noreply.github.com>
2021-04-29 11:27:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
 var game;
-window.onload=function()
-{
-  var config = {
+
+
+
+window.onload = function() {
+    var isMobile = navigator.userAgent.indexOf("Mobile");
+    if (isMobile == -1) {
+        isMobile = navigator.userAgent.indexOf("Tablet");
+    }
+    //
+    //
+    if (isMobile == -1) {
+        var config = {
+            type: Phaser.AUTO,
+            width: 480,
+            height: 640,
+            parent: 'phaser-game',
+            scene: [SceneMain]
+        };
+    } else {
+        var config = {
         type: Phaser.AUTO,
-        width: 480,
-        height: 640,
+        width: window.innerWidth,
+        height: window.innerHeight,
         parent: 'phaser-game',
         scene: [SceneMain]
     };
+    }
+
+
+
+
+
      game = new Phaser.Game(config);
 }
